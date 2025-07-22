8e28503e21cc60404468503fa4e28f0a1a72c17c
etsayhaben <152095961+etsayhaben@users.noreply.github.com>
2025-07-22 15:14:39 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/haben_etsay_portfolio/', // ← repo name
+  base: '/haben_etsay_portfolio/', // <-- add this line
+  plugins: [react()]
 })
